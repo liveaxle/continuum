ae4ab065fb08d86648c94db6da237b9a139dd948
Ken Stowell <kenstowell@Ken-Stowell.local>
2017-12-22 12:25:36 -0800

    First pass at adding mocha/chai as the unit test framework.

---
+++ tests/unit/resource/index.js
@@ -0,0 +1,80 @@
+'use strict';
+
+/***********************************************************************************************************************************************
+ * CONTINUUM TESTS - UNIT - RESOURCE
+ ***********************************************************************************************************************************************
+ * @description
+ */
+const chai = require('chai');
+const Resource = using('src/resource');
+const Response = using('src/resource/response');
+
+
+//
+// Chai constants
+//------------------------------------------------------------------------------------------//
+// @description
+//
+const expect = chai.expect;
+const assert = chai.assert;
+
+
+//
+// USERS RESOURCE
+//------------------------------------------------------------------------------------------//
+// @description
+//
+const Users = new Resource('users', {
+  base: process.env.API
+});
+
+//
+// RESOURCE RETRIEVAL
+//------------------------------------------------------------------------------------------//
+// @description
+//
+describe('Continuum - Resource - Retrieval', () => {
+  
+  let request = Users.get({params: {_sort: 'created'}, headers: {'Continuum-Request-Header': Math.random().toString(32).substr(2, 16)}});
+
+  request.then(()=> {}, (e) => {
+    if(e.message.match('ECONNREFUSED')) {
+      throw new Error('Continuum - to run the unit tests you need to have the demo server running. `npm run demo:seed && npm run demo:server`');
+    }
+  });
+
+  it('Should Return an instance of Response', (done) => {
+    request.then(response => {
+      expect(response instanceof Response).to.equal(true);
+    }).then(done);
+  });
+
+  it('Should contain user data from /users', (done) => {
+    request.then(response => {
+      expect(response.data).to.not.equal(undefined);
+      expect(response.data).to.not.equal(null);
+      expect(response.data).to.have.length.above(0);
+    }).then(done);
+  });
+
+  it('Should contain params given to the request wrapper', (done) => {
+    request.then(response => {
+      expect(response.config.params._sort).to.equal('created');
+    }).then(done);
+  });
+
+  it('Should contain headers given to the request wrapper', (done) => {
+    request.then(response => {
+      expect(response.config.headers['Continuum-Request-Header']).to.not.equal(undefined);
+    }).then(done);
+  });
+
+  it('Should execute error handlers on errant status codes', (done) => {
+    Users.get({url: '/merp'})
+      .then(response => {
+        throw new Error('Success Handler was called.');
+      }).catch(e => {
+        expect(e).to.not.equal(null)
+      }).then(done);
+  });
+})
